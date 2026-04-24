da6066e2e5daf3f0c6c685a96e74ec86d0724619
artroobio <absarkhan77@gmail.com>
2026-04-24 15:22:09 +0530

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  },
+  vite: {
+    build: {
+      cssMinify: true
+    },
+    css: {
+      devSourcemap: false
+    }
+  }
+});
